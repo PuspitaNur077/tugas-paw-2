b5b787872016353808e8ba0761d3978fd5afdf87
PuspitaNur077 <101179791+PuspitaNur077@users.noreply.github.com>
2022-03-13 18:21:53 +0700

    Add files via upload

---
+++ puspita/coba.php
@@ -0,0 +1,5 @@
+<?php
+	echo "Puspita Nur Indah Sari Ramadana";
+	echo "<br />";
+	echo "NRP 180411100077	";  
+?>
---
+++ puspita/coba2.php
@@ -0,0 +1,22 @@
+<?php
+
+function perkenalan(){
+  echo "Assalamulaikmu, ";
+  echo "Perkenalkan, nama saya Puspita Nur Indah Sari Ramadana<br/>";
+  echo "Senang berkenalan dengan anda<br/>";
+}
+
+
+perkenalan();
+
+echo "<hr>";
+
+
+perkenalan();
+
+echo "<hr>";
+
+
+perkenalan();
+
+?>
